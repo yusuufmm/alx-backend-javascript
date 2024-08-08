e177f3a64d1c3a3e6467905e9a891e21a271d45a
Yusuf Muhammad Musa <yusuf2000mm@gmail.com>
2024-08-08 12:17:58 +0000

    updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,4 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, 
-reject) => {
-	  // Example: resolve with a string after a delay
-    setTimeout(() => {
-      resolve('Response from API');
-    }, 1000);
-  });}
+  return new Promise(() => {});
+}
