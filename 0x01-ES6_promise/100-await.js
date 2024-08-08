c9060ef3134a7742ddb746496152cda388d05550
Yusuf Muhammad Musa <yusuf2000mm@gmail.com>
2024-08-08 04:54:49 +0000

    ES6 Js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,12 @@
 export default async function asyncUploadUser() {
   let res = {};
+
  try {
     const photo = await uploadPhoto();
     const user = await createUser();
     res = { photo, user };
   } catch (err) {
-    res = { photo: null, user: null };
+    res = { photo: null, user: null };7
   }
   return res;
 }
